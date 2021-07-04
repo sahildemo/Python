9841d467e3befa6f6ed9e3e67a4e7bc64be686d5
sahil <sahilchauhan786@gmail.com>
2021-07-04 14:15:39 +0530

    Commit1: 9 july 20_2:52pm

---
+++ class_36th(myGit2).py
@@ -0,0 +1,44 @@
+"New Topic = myGit_2nd"
+
+"""
+=====> Abhi meri file "green color" mai dikh rhi h, iska mtlb h ke meri 
+       file git ke sath interface toh h, bt abhi maine koi bhi 
+       commit ni kiya h abhi tk, jase hi hum commit krdenge wase hi
+       iska color change ho jyga.
+
+=====> Git, is a tool for "version control".
+"""
+
+
+
+
+"""Git tool for version control:
+=====> Through 'Git' we save different versions of our projects. Whenever 
+       we need any version that is, we want to resolve the latest changes
+       from the program, we can do through 'Git'.
+
+=====> Every 'commit' means "new version" created.
+"""
+
+
+
+
+"""
+=====> Ab hum cahte h ke hum apne pycharm project ko directly Github pr
+       directly upload krna. 
+
+=====> Apko file retrieve krni h toh ap "Rollback" pr click kre git menu mai.
+
+=====> Ab hume git mai add krne ki jarurt ni humne file new bnyi tbhi 
+       integrate pr click krdiya.
+
+=====> Directly ap git mai commit krnge.
+"""
+
+
+
+
+# New Program
+print("CETPA")
+
+print("HELLO")
